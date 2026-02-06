ac4c0e6ee58b3e97ac2fbd8e2ee2a91c2fb2fbb2
Shai Weinstein <s@s.com>
2026-02-05 19:47:43 -0500

    update values

--- opendbc/car/volkswagen/values.py
+++ opendbc/car/volkswagen/values.py
@@ -61,10 +61,10 @@ class CarControllerParams:
   STEER_DRIVER_FACTOR = 1                  # from dbc
 
   STEER_TIME_MAX = 360                     # Max time that EPS allows uninterrupted HCA steering control
-  STEER_TIME_BM = 120                      # Attempts to mitigate the EPS max steer timer begin
+  STEER_TIME_BM = 220                      # Attempts to mitigate the EPS max steer timer begin
   STEER_TIME_ALERT = STEER_TIME_MAX - 10   # If mitigation fails, time to soft disengage before EPS timer expires
   STEER_LOW_TORQUE = int(STEER_MAX * 0.18) # Steer timer mitigation performed when torque output under 20%
-  STEER_TIME_LOW_TORQUE = 1.8              # Wait for this duration of STEER_LOW_TORQUE to begin mitigation
+  STEER_TIME_LOW_TORQUE = 0.7              # Wait for this duration of STEER_LOW_TORQUE to begin mitigation
   STEER_TIME_STUCK_TORQUE = 1.9            # EPS limits same torque to 6 seconds, reset timer 3x within that period
   STEER_TIME_RESET = 1.1                   # Duration of HCA disable needed for effective EPS timer reset
 
